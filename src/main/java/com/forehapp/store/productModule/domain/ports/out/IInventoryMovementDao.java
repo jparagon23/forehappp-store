0fd35628afd6ce0a7a4ae4577c23eed8fd31ec62
JUAN PABLO ARAGON <jparagon2021@gmail.com>
2026-05-13 11:37:26 -0500

    Feat: Seller panel inventory

--- src/main/java/com/forehapp/store/productModule/domain/ports/out/IInventoryMovementDao.java
+++ src/main/java/com/forehapp/store/productModule/domain/ports/out/IInventoryMovementDao.java
@@ -1,8 +1,12 @@
 package com.forehapp.store.productModule.domain.ports.out;
 
 import com.forehapp.store.productModule.domain.model.InventoryMovement;
+import com.forehapp.store.productModule.domain.model.MovementReason;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IInventoryMovementDao {
     InventoryMovement save(InventoryMovement movement);
     void incrementStock(Long variantId, int delta);
+    Page<InventoryMovement> findByVariant(Long variantId, MovementReason reason, Pageable pageable);
 }
